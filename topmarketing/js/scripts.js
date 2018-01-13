fa8658a9cdb1733e2b0c1e64e7f204c3c6fb161c
IlyaGremitskikh <gremilya@gmail.com>
2018-01-13 23:46:10 +0500

    Replaced New Version

--- topmarketing/js/scripts.js
+++ topmarketing/js/scripts.js
@@ -1,41 +1,4 @@
 $(document).ready(function() {
-
-    $('.image-popup-vertical-fit').magnificPopup({
-        type: 'image',
-        closeOnContentClick: true,
-        mainClass: 'mfp-img-mobile',
-        image: {
-            verticalFit: true
-        }
-
-    });
-
-    $('.image-popup-fit-width').magnificPopup({
-        type: 'image',
-        closeOnContentClick: true,
-        closeBtnInside: false,
-        fixedContentPos: true,
-        mainClass: 'mfp-no-margins mfp-with-zoom',
-        image: {
-            verticalFit: false
-        }
-    });
-
-    $('.image-popup-no-margins').magnificPopup({
-        type: 'image',
-        closeOnContentClick: true,
-        closeBtnInside: false,
-        fixedContentPos: true,
-        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-        image: {
-            verticalFit: false
-        },
-        zoom: {
-            enabled: true,
-            duration: 300 // don't foget to change the duration also in CSS
-        }
-    });
-
     $('.values').slick({
         autoplay: true,
         autoplaySpeed: 2000,
@@ -65,16 +28,19 @@ $(document).ready(function() {
         return false;
     });
 
-    var typed = new Typed('.typed', {
-        strings: ['Мы ^500 брендинговое <br>агентство нового <br>формата', 'Мы ^500 брендинговое <br>агентство нового <br>форматa', 'Мы ^500 брендинговое <br>агентство нового <br>формата', 'Мы ^500 брендинговое <br>агентство нового <br>форматa', 'Мы ^500 брендинговое <br>агентство нового <br>формата', 'Мы ^500 брендинговое <br>агентство нового <br>форматa', 'Мы ^500 брендинговое <br>агентство нового <br>формата', 'Мы ^500 брендинговое <br>агентство нового <br>форматa', 'Мы ^500 брендинговое <br>агентство нового <br>формата', 'Мы ^500 брендинговое <br>агентство нового <br>форматa'],
-        typeSpeed: 30,
-        backSpeed: 40,
-        smartBackspace: true, // this is a default
-        loop: true,
-        backDelay: 800,
-        startDelay: 300,
-    });
-
     var scroll = new SmoothScroll('a[href*="#"]');
 
+$(function(){
+    // Trigger maximage
+    jQuery('#maximage').maximage({
+        cycleOptions: {
+            fx: 'fade',
+            speed: 1000, 
+            timeout: 5000,
+            prev: '#arrow_left',
+            next: '#arrow_right',
+            pause: 1
+        }
+    });
+});
 });
