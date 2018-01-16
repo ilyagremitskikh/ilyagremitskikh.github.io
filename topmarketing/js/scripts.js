90a603a8e8d8cde0434f93f65958d11366fccbee
IlyaGremitskikh <gremilya@gmail.com>
2018-01-16 16:28:29 +0500

    Edits

--- topmarketing/js/scripts.js
+++ topmarketing/js/scripts.js
@@ -46,11 +46,15 @@ $(document).ready(function() {
     $(window).scroll(function() {
         if ($(window).scrollTop() > 620) {
             $("#logo").attr("src", "img/logo-scrolled.png"),
-            $(".navbar").css({padding: '0', 'transition': 'padding 0.5s ease'})
+                $(".navbar").css({ padding: '0', 'transition': 'padding 0.5s ease' }),
+                $(".navbar-nav").css('margin-bottom', '0'),
+                $(".nav-link").css({ fontSize: '80%', 'padding': '0 30px' })
         };
         if ($(window).scrollTop() <= 620) {
             $("#logo").attr("src", "img/logo.png"),
-            $(".navbar").css('padding', '.5rem 1rem')
+                $(".navbar").css('padding', '.5rem 1rem'),
+                $(".navbar-nav").css('margin-bottom', '20px'),
+                $(".nav-link").css({ fontSize: '18px', 'padding': '0 20px' })
         }
     });
     $(window).scroll(function() {
@@ -61,4 +65,27 @@ $(document).ready(function() {
             $(".navbar").removeClass('scrolled')
         }
     });
+    $('header').flowtype({ minFont: 16, maxFont: 30, fontRatio: 55 });
+
+    // Создание обработчика для события window.onLoad
+    YMaps.jQuery(function() {
+        // Создание экземпляра карты и его привязка к созданному контейнеру
+        var map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]);
+
+        // Установка для карты ее центра и масштаба
+        map.setCenter(new YMaps.GeoPoint(61.379880, 55.152536), 17);
+
+        // Создание стиля для значка метки
+        var s = new YMaps.Style();
+        s.iconStyle = new YMaps.IconStyle();
+        s.iconStyle.href = "../img/map-icon.png";
+        s.iconStyle.size = new YMaps.Point(48.8, 40);
+        s.iconStyle.offset = new YMaps.Point(-5, -50);
+        map.addControl(new YMaps.Zoom());
+        // Создание метки и добавление ее на карту
+        var placemark = new YMaps.Placemark(map.getCenter(), { style: s, hasBalloon: false });
+        map.addOverlay(placemark);
+    });
+
+
 });
